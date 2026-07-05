f42f8a88fb1fe66337b2d7cc3830ce070f2d3dbb
jamiz qamar <jamizalam1@gmail.com>
2026-07-05 11:46:35 +0530

    logic build

--- Logic_build/phase1.py
+++ Logic_build/phase1.py
@@ -19,21 +19,67 @@ else:
 year = int(input("enter the year : "))
 if year % 400 == 0:
     print("its a leap year ")
-elif year % 100 != 0:
+elif year % 100 == 0:
     print("its a not a leap year ")
-elif year % 4 == 0:
+elif year % 4 == 0 and year % 100 != 0:
     print("its a leap year ")
 else:
     print("its a not a leap year ")
 
 
 # Take three number and print the largest 
+a = int(input("enter the no.a : "))
+b = int(input("enter the no.b : "))
+c = int(input("enter the no.c : "))
+if a>b and a>c:
+    print("a is largest")
+elif b>a and b>c:
+    print("b is largest")
+elif c>a and c>b:
+    print("c is largest")
+else:
+    print("a,b,c are equall")
+
 
 # Take a character and check whether it is vowel or consonant
+character = input("enter the character : ")
+vowel = ['a','e','i','o','u','A','E','I','O','U']
+if character in vowel:
+    print("its a vowel")
+else:
+    print("its a consonant")
+
 # Take a character and check whether it’s uppercase, lowercase, a digit, or a special character
+if character == character.upper():
+    print("its uppercase")
+elif character == character.lower():
+    print("its lowercase")
+elif character.isdigit():
+    print("its a digit")
+else:
+    print("its a special character")
+
+
 # Take three sides and check if they form a valid triangle
+if a+b > c:
+    print("triangle is valid")
+elif b+c > a:
+    print ("triangle is valid")
+elif c+a>b:
+    print("triangle is valid")
+else:
+    print("trinangle is invalid")
+
+
 # If the sides form a valid triangle, determine whether it is equilateral, isosceles, or scalene
-# Take marks (0–100) and print the corresponding grade (A/B/C/D/F)
+if a==b==c:
+    print("triangle is equilateral")
+elif a == b or b == c or a == c:
+    print ("triangle is isosceles")
+else:
+    print("trinangle is scalene")
+
+
 # Take a month number (1–12) and print the number of days in that month (ignore leap years)
 # Take a day number (1–7) and print the corresponding day name
 # Take a 3-digit number and check if all digits are distinct
