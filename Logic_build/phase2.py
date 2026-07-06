a3282f80666b9fc869e12f2aa79905b141b081a8
jamiz qamar <jamizalam1@gmail.com>
2026-07-06 23:11:08 +0530

    logic build

---
+++ Logic_build/phase2.py
@@ -0,0 +1,9 @@
+# Check if a number is an Armstrong number
+# Print all Armstrong numbers between 1 and 1000
+# Print all prime numbers between 1 and N
+# Print the reverse of a number (123 → 321)
+# Check if a number is perfect
+# Check if two strings are anagrams (without using collections)
+# Check if two strings are rotations of each other
+# Find the word with maximum vowels in a sentence
+# Remove duplicate words from a sentence
